def26b766a88dddf8c71012871ce10136710b154
github-action-benchmark <github@users.noreply.github.com>
2022-09-11 13:01:06 +0000

    add otterdb benchmarks (go) benchmark result for 4acd8211de7ffddd7ef0bbf6979a8100010c5822

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1662901266706,
+  "repoUrl": "https://github.com/PandaSekh/otterdb",
+  "entries": {
+    "otterdb benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "alessio@alessiofranceschi.dev",
+            "name": "Alessio Franceschi",
+            "username": "PandaSekh"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4acd8211de7ffddd7ef0bbf6979a8100010c5822",
+          "message": "Update benchmarks.yml\n\nSigned-off-by: Alessio Franceschi <alessio@alessiofranceschi.dev>",
+          "timestamp": "2022-09-11T15:00:39+02:00",
+          "tree_id": "8d3671b75db5a256f136da46f4a05e1d4eb9663f",
+          "url": "https://github.com/PandaSekh/otterdb/commit/4acd8211de7ffddd7ef0bbf6979a8100010c5822"
+        },
+        "date": 1662901266335,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFnvHash",
+            "value": 133.5,
+            "unit": "ns/op",
+            "extra": "8829910 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
